1d1303f320f7043cd52a3d0eca72e92cc6035306
GhostFantik <ghostfantik@gmail.com>
2019-09-08 18:39:30 +0500

    config files and dockerfiles were updated!

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@
 module.exports = {
     db : 'postgres://postgres:123456789Egor@db:5432/deliveryDB',
     port: 3000,
-    frontendUrl: 'http://192.168.99.100:80',
+    frontendUrl: 'http://localhost:80',
     jwtSecretKey: 'fantik',
     // eslint-disable-next-line no-undef
     modelsFolder: (`${__dirname}/Models/`),
