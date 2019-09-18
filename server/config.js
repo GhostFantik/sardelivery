c9a6c0613ffae8b16669c4de997d6cb4dde2ccf2
Egor <ghostfantik@gmail.com>
2019-09-18 23:54:54 +0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@
 module.exports = {
     db : 'postgres://postgres:123456SarDelivery@db:5432/deliveryDB',
     port: 3000,
-    frontendUrl: 'http://localhost:80',
+    frontendUrl: 'http://194.87.101.85:80',
     jwtSecretKey: 'fantik',
     // eslint-disable-next-line no-undef
     modelsFolder: (`${__dirname}/Models/`),
