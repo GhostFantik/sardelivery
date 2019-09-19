8d5ef4f10e97273e9c67a5ab0ac62306580ede93
GhostFantik <ghostfantik@gmail.com>
2019-09-20 00:24:11 +0500

    [Vk] Fix notification 9

--- VkBot/AdminNotification.js
+++ VkBot/AdminNotification.js
@@ -3,7 +3,7 @@ const methods = require('./VkAPI/methodsManager');
 
 exports.newOrder = async function(data) {
     const response = await methods.sendMessage({
-        chatId: config.Vk.admin_id,
+        chatId: config.Vk.chat_id,
         text: `Внимание! Новый заказ! Срочно обработать!
                 Data: ${data}`,
     });
